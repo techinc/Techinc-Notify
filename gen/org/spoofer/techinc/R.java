500e77191ff11b74c52d351eda16583d263f5445
Arien Holthuizen <arienh4@arienh4.net>
2012-08-27 15:19:54 +0200

    Provisionally finished main interface design

--- gen/org/spoofer/techinc/R.java
+++ gen/org/spoofer/techinc/R.java
@@ -16,23 +16,28 @@ public final class R {
         public static final int techinclogo_mono=0x7f020002;
     }
     public static final class id {
-        public static final int apply=0x7f06000f;
-        public static final int boot_start=0x7f060003;
-        public static final int butn_browse_sound=0x7f060007;
-        public static final int label_open_url=0x7f060001;
-        public static final int label_pref_notify_sound=0x7f060005;
-        public static final int label_pref_update_freq=0x7f060008;
-        public static final int label_update_url=0x7f06000a;
-        public static final int menu_settings=0x7f060010;
-        public static final int notify_sound=0x7f060006;
-        public static final int open_url=0x7f060002;
-        public static final int reset=0x7f06000e;
-        public static final int start=0x7f06000c;
-        public static final int stop=0x7f06000d;
-        public static final int toggle=0x7f060000;
-        public static final int update_freq=0x7f060009;
-        public static final int update_url=0x7f06000b;
-        public static final int vibrate=0x7f060004;
+        public static final int apply=0x7f060014;
+        public static final int boot_start=0x7f060008;
+        public static final int butn_browse_sound=0x7f06000c;
+        public static final int label_open_url=0x7f060006;
+        public static final int label_pref_notify_sound=0x7f06000a;
+        public static final int label_pref_update_freq=0x7f06000d;
+        public static final int label_text=0x7f060000;
+        public static final int label_update_url=0x7f06000f;
+        public static final int menu_settings=0x7f060015;
+        public static final int monitoring=0x7f060004;
+        public static final int notify_sound=0x7f06000b;
+        public static final int open_url=0x7f060007;
+        public static final int refresh=0x7f060003;
+        public static final int reset=0x7f060013;
+        public static final int start=0x7f060011;
+        public static final int status=0x7f060002;
+        public static final int status_label=0x7f060001;
+        public static final int stop=0x7f060012;
+        public static final int toggle=0x7f060005;
+        public static final int update_freq=0x7f06000e;
+        public static final int update_url=0x7f060010;
+        public static final int vibrate=0x7f060009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -44,14 +49,22 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int button_browse=0x7f04000d;
+        public static final int button_refresh=0x7f040017;
         public static final int button_start_service=0x7f04000b;
         public static final int button_stop_service=0x7f04000c;
+        public static final int closed=0x7f040015;
         public static final int confirm_no=0x7f040010;
         public static final int confirm_reset=0x7f04000e;
         public static final int confirm_yes=0x7f04000f;
+        public static final int disable=0x7f04001a;
+        public static final int enable=0x7f04001b;
+        public static final int main_text=0x7f040012;
         public static final int menu_settings=0x7f040011;
+        public static final int monitoring_disabled=0x7f040019;
+        public static final int monitoring_enabled=0x7f040018;
         public static final int notify_closed=0x7f040002;
         public static final int notify_open=0x7f040001;
+        public static final int open=0x7f040014;
         public static final int pref_apply=0x7f040008;
         public static final int pref_boot_start=0x7f04000a;
         public static final int pref_notify_sound=0x7f040009;
@@ -60,5 +73,7 @@ public final class R {
         public static final int pref_reset=0x7f040006;
         public static final int pref_update_freq=0x7f040003;
         public static final int pref_update_url=0x7f040004;
+        public static final int status=0x7f040013;
+        public static final int unknown=0x7f040016;
     }
 }
